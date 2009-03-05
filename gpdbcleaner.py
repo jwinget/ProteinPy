f5aeac5c15f1946e4abd112d7201620a7171325d
Jason Winget <jason@localhost.localdomain>
2009-03-05 10:23:54 -0800

    Added record name extraction when opening PDB file

--- gpdbcleaner.py
+++ gpdbcleaner.py
@@ -2,10 +2,11 @@
 
 # http://biostumblematic.wordpress.com
 
-import sys, re, tkFileDialog
+import sys, re, tkFileDialog, string
 from Tkinter import *
 
 filelist=[]
+recordnames = []
 
 class Gpdbcleaner:
     def __init__(self, root):
@@ -13,17 +14,21 @@ class Gpdbcleaner:
         frame = Frame(root)
         frame.pack()
 
-        self.openbutton = Button(frame, text='Select PDB file', command=self.askopenfilename)
+        self.openbutton = Button(frame, text='Select PDB file', command=self.openfile)
         self.openbutton.pack(side=LEFT)
 
         self.savebutton = Button(frame, text='Save As', command=self.asksaveasfilename)
         self.savebutton.pack(side=LEFT)
 
         self.runbutton = Button(frame, text='Run', fg='green', command=self.run)
-        self.runbutton.pack(side=LEFT)
+        self.runbutton.pack(side=TOP)
 
         self.quitbutton = Button(frame, text='Quit', fg='red', command=frame.quit)
         self.quitbutton.pack(side=LEFT)
+
+    def openfile(self):
+	self.askopenfilename()
+	self.getrecordnames()
 
     def askopenfilename(self):
         '''Get the name of the PDB file to be cleaned up'''
@@ -34,6 +39,19 @@ class Gpdbcleaner:
         '''Get the save as file name'''
         cleanfile = tkFileDialog.asksaveasfilename()
         return filelist.append(cleanfile)
+
+    def getrecordnames(self):
+	'''Create a list of all the record names in the PDB file'''
+        inputfile = open(filelist[0], 'r')
+	lines = inputfile.readlines()
+	for line in lines:
+	    recordmatch = re.match('^\S*', line)
+	    record = recordmatch.group()
+	    if record in recordnames:
+	        pass
+	    else:
+	        recordnames.append(record)
+	inputfile.close()
 
     def run(self):
         '''Perform the cleanup'''
