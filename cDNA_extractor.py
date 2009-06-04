e734a14ca4a4595a24cc4712aab1a6823bf5589b
Jason Winget <jwinget@gmail.com>
2009-06-04 13:09:50 -0700

    Fixed a bug in writing output, added status messages

--- cDNA_extractor.py
+++ cDNA_extractor.py
@@ -13,14 +13,15 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 
 reader = csv.reader(open('test.csv'))
+extracted = []
+j=0
+
 for row in reader:
     input_prot = row[0]
     get_prot = ExPASy.get_sprot_raw(input_prot)
-#    prot_obj = SeqIO.read(get_prot, "swiss")
     prot_record = SwissProt.read(get_prot)
     get_prot.close()
     prot_seq = prot_record.sequence
-#    prot_seq = prot_obj.seq
     prot_gene = prot_record.gene_name
     prot_domain = prot_seq[int(row[1])-1:int(row[2])]
     cdna = Seq(row[3], IUPAC.unambiguous_dna)
@@ -30,7 +31,6 @@ for row in reader:
     i=0
     # Steps through each possible frame of the input cDNA
     while i < 3:
-        extracted = []
         frame = cdna[i::]
         trans = frame.translate()
         orf_find = re.search(str(prot_domain), str(trans))
@@ -40,10 +40,13 @@ for row in reader:
             cdna_start = len(trans_split[0])*3
             cdna_stop = cdna_start + len(trans_split[1])*3
             cdna_extracted = frame[cdna_start:cdna_stop]
-            extracted.append(prot_gene)
-            extracted.append(cdna_extracted)
-            writer.writerow(extracted)
+            row = prot_gene+','+str(cdna_extracted)
+            extracted.append(row)
         else:
             pass
         i += 1
+    j += 1
+    print 'Extracted cDNA from protein '+str(j)
+for item in extracted:
+    writer.writerow(extracted)
 outputfile.close()
